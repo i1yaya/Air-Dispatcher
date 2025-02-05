9ea30e45d8e98fe908d4b1c9741e480e696431d2
i1yaya <yhurt100@gmail.com>
2025-02-05 22:26:36 +0300

    Initial Commit

---
+++ AirfieldDispatcher/AirfieldDispatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirfieldDispatcherApp.swift
+//  AirfieldDispatcher
+//
+//  Created by Ilya Ruyatkin on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirfieldDispatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirfieldDispatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirfieldDispatcher
+//
+//  Created by Ilya Ruyatkin on 05.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
